0ac5e8c140fbce78833774abe3e121894c9bc0e4
Aarón Espí <aaronespi@MacBook-Pro-de-Aaron.local>
2025-01-16 13:44:50 +0100

    itemPrice locator fix

--- src/constants/locators/ItemLocators.ts
+++ src/constants/locators/ItemLocators.ts
@@ -1,6 +1,6 @@
 export class ItemLocators {
   static readonly itemDetailsContainer = '.inventory_details_container';
   static readonly itemName = '[data-test="inventory-item-name"]';
-  static readonly itemPrice = '[data-test="inventory-item-name"]';
+  static readonly itemPrice = '[data-test="inventory-item-price"]';
   static readonly itemDescription = '[data-test="inventory-item-desc"]';
 }
