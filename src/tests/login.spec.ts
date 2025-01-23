1117e101c0481abc085f44c5c8ac0a7646b418a2
ESPI BERNAL Aaron <aaron.espi@soprasteria.com>
2025-01-23 10:41:33 +0100

    initialize page objects in beforeEach

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -3,45 +3,43 @@ import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
 import { config } from '../../playwright.config';
 
+let loginPage: LoginPage;
+let inventoryPage: InventoryPage;
+
 test.beforeEach(async ({ page }) => {
-  const loginPage = new LoginPage(page);
+  loginPage = new LoginPage(page);
+  inventoryPage = new InventoryPage(page);
+
   await loginPage.goto();
 });
 
-test('should log in successfully with valid credentials', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
+test('should log in successfully with valid credentials', async () => {
   await loginPage.fillUsernameInput(config.validUsername);
   await loginPage.fillPasswordInput(config.validPassword);
   await loginPage.clickOnLogin();
   await inventoryPage.verifyHomePageLoaded();
 });
 
-test('should not log in with invalid credentials', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
+test('should not log in with invalid credentials', async () => {
   await loginPage.fillUsernameInput(config.invalidUsername);
   await loginPage.fillPasswordInput(config.invalidPassword);
   await loginPage.clickOnLogin();
   await loginPage.verifyInvalidCredentialsMessage();
 });
 
-test('should not log in with empty username', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+test('should not log in with empty username', async () => {
   await loginPage.fillPasswordInput(config.validPassword);
   await loginPage.clickOnLogin();
   await loginPage.verifyUsernameRequiredMessage();
 });
 
-test('should not log in with empty password', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+test('should not log in with empty password', async () => {
   await loginPage.fillUsernameInput(config.validUsername);
   await loginPage.clickOnLogin();
   await loginPage.verifyPasswordRequiredMessage();
 });
 
-test('should not log in with a blocked user account', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+test('should not log in with a blocked user account', async () => {
   await loginPage.fillUsernameInput(config.lockedUsername);
   await loginPage.fillPasswordInput(config.validPassword);
   await loginPage.clickOnLogin();
--- src/tests/side-menu.spec.ts
+++ src/tests/side-menu.spec.ts
@@ -5,49 +5,48 @@ import { AboutPage } from '../pages/AboutPage';
 import { SideMenuAction } from '../constants/enum/SideMenuAction';
 import { config } from '../../playwright.config';
 
+let loginPage: LoginPage;
+let inventoryPage: InventoryPage;
+let aboutPage: AboutPage;
+
 test.beforeEach(async ({ page }) => {
-  const loginPage = new LoginPage(page);
+  loginPage = new LoginPage(page);
+  inventoryPage = new InventoryPage(page);
+  aboutPage = new AboutPage(page);
+
   await loginPage.login(config.validUsername, config.validPassword);
 });
 
-test('should reveal side menu when hamburger is clicked', async ({ page }) => {
-  const inventoryPage = new InventoryPage(page);
+test('should reveal side menu when hamburger is clicked', async () => {
   await inventoryPage.toggleSideMenu(SideMenuAction.Open);
   await inventoryPage.verifySideMenuIsOpen();
 });
 
-test('should display all side menu options when opened', async ({ page }) => {
-  const inventoryPage = new InventoryPage(page);
+test('should display all side menu options when opened', async () => {
   await inventoryPage.toggleSideMenu(SideMenuAction.Open);
   await inventoryPage.verifySideMenuOptionsAreVisible();
 });
 
-test('should reset app state when reset button is clicked', async ({ page }) => {
-  const inventoryPage = new InventoryPage(page);
+test('should reset app state when reset button is clicked', async () => {
   await inventoryPage.addProductToCart(0);
   await inventoryPage.toggleSideMenu(SideMenuAction.Open);
   await inventoryPage.clickOnResetAppState();
   await inventoryPage.verifyCartIsEmpty();
 });
 
-test('should collapse side menu when hamburger icon is clicked again', async ({ page }) => {
-  const inventoryPage = new InventoryPage(page);
+test('should collapse side menu when hamburger icon is clicked again', async () => {
   await inventoryPage.toggleSideMenu(SideMenuAction.Open);
   await inventoryPage.toggleSideMenu(SideMenuAction.Close);
   await inventoryPage.verifySideMenuIsClosed();
 });
 
-test('should navigate to login page after clicking logout button in side menu', async ({ page }) => {
-  const inventoryPage = new InventoryPage(page);
-  const loginPage = new LoginPage(page);
+test('should navigate to login page after clicking logout button in side menu', async () => {
   await inventoryPage.toggleSideMenu(SideMenuAction.Open);
   await inventoryPage.clickOnLogout();
   await loginPage.verifyLoginPageLoaded();
 });
 
-test('should navigate to about page after clicking about button in side menu', async ({ page }) => {
-  const inventoryPage = new InventoryPage(page);
-  const aboutPage = new AboutPage(page);
+test('should navigate to about page after clicking about button in side menu', async () => {
   await inventoryPage.toggleSideMenu(SideMenuAction.Open);
   await inventoryPage.clickOnAbout();
   await aboutPage.verifyAboutPageLoaded();
